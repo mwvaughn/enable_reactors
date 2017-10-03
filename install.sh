0a3b60232f6b4982110ef5f1fc2605d99ad7da16
Matt Vaughn <vaughn@tacc.utexas.edu>
2017-10-02 19:28:38 -0500

    Merp. Sudo doesn't exist inside containers by default

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash 
 
-sudo apt-get -y update && \
-     apt-get install python-setuptools \
-                     python-dev \
-                     build-essential \
-                     python-pip && \
-     apt-get clean
+apt-get -y update && \
+apt-get install python-setuptools \
+                python-dev \
+                build-essential \
+                python-pip && \
+apt-get clean
 
-sudo pip install -r requirements.txt
-sudo cp -R agavepy /
+pip install -r requirements.txt
+cp -R agavepy /
